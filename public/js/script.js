2f51441d80698082504d6dc8826a66bd0c6f53d6
Arber Jonuzi <arber.jonuzi@etudiant.univ-reims.fr>
2023-07-13 19:54:56 +0200

    New version

---
+++ public/js/script.js
@@ -0,0 +1,34 @@
+
+// function setBackgroundBasedOnTime() {
+//     const header = document.querySelector('.header');
+//     const currentTime = new Date().getHours();
+//
+//     if (currentTime >= 9 && currentTime < 18) {
+//         // Daytime
+//         header.style.backgroundImage = "url('public/img/1.jpg')";
+//
+//     } else if (currentTime >= 18 && currentTime < 20) {
+//         // Sunset
+//         header.style.backgroundImage = "url('public/img/sunset.png')";
+//     } else if (currentTime >= 5 && currentTime < 9){
+//
+//     } else {
+//         // Nighttime
+//         header.style.backgroundImage = "url('public/img/night.png')";
+//     }
+// }
+//
+// setBackgroundBasedOnTime(); // Set initial background based on current time
+//
+// // Update background image every minute to handle changing time
+// setInterval(setBackgroundBasedOnTime, 60000);
+
+function redirectToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+        setTimeout(function() {
+            window.scrollBy(0, -150); // Scroll up by 30 pixels
+        }, 500); // Adjust the delay as needed
+    }
+}
